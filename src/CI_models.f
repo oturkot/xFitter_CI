ca6de2e82b8ec04d3aa60b5eab412bb2cacfd2bb
shedprog <flynik75@gmail.com>
2018-04-18 14:30:34 +0300

    cant merge

--- src/CI_models.f
+++ src/CI_models.f
@@ -642,7 +642,9 @@ CCC------------HERE WE FORM ETA FROM PAR
      $ par(1), par(2), par(3), par(4), par(5), par(6), par(7), par(8),
      $ par(1), par(2), par(3), par(4), par(5), par(6), par(7), par(8)  
      $], [4,6])
+
 C       write (190,*) 'CIvarval (in ModImpose)=',CIvarval
+
         
       Return
       End
